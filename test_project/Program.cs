c36a439e7cc39574f044b36677f283ae9894b594
mikhailo2000 <mihamelnik077@gmail.com>
2026-09-22 16:20:11 +0300

    test

--- test_project/Program.cs
+++ test_project/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+int result=0;
+int a = 5;
+int b = 10;
+result = a + b;
+
+
+var result1= 1.5;
+result1 = result *5;
+Console.WriteLine("The result is: " + result);
+Console.WriteLine("The result is: " + result1);
