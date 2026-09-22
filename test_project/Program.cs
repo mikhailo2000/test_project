e3e3c85080d5e6b2dd5c8fa85f99dd0c4960958e
mikhailo2000 <mihamelnik077@gmail.com>
2026-09-22 16:30:06 +0300

    Update Program.cs

--- test_project/Program.cs
+++ test_project/Program.cs
@@ -1,11 +1,17 @@
 ﻿Console.WriteLine("Hello, World!");
 int result=0;
+int c = 10 / 5;
 int a = 5;
 int b = 10;
-result = a + b;
+int f = 10 - 5;
+result = a + b / c;
 
 
 var result1= 1.5;
 result1 = result *5;
+var result2 = result1 / 10;
+var result3 = result2 - 10;
 Console.WriteLine("The result is: " + result);
 Console.WriteLine("The result is: " + result1);
+Console.WriteLine("The result is: " + result2);
+Console.WriteLine("The result is: " + result3);
